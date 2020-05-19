6758be8536843978c4e4735b6771e72ccf69a29b
Pignataro67 <Pignataro67@gmail.com>
2020-05-19 11:58:38 -0700

    refractoring

--- client/src/components/Goals/GoalsList.js
+++ client/src/components/Goals/GoalsList.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import Goal from './Goal';
 
-export default class GoalsList extends Component {
-  render(){
-    const { goals }  = this.props; 
+const GoalsList = props => {
+  const { goals }  = this.props; 
 
-    const GoalsList = ({ goals }) => (
-        <div>	
-            <h3>Your Prespires to Aspire</h3> 
-          <ol>	  
-          {goals.map((goal) => (
-            <li>  
-              <Goal aim={goal.aim} aim={goal.aim} goal={goal} strategy={goal.strategy} 
-            category={goal.category} objectives={goal.objectives} />
-        </li> ))}
+  const GoalsList = props.goals.map(goal => <li><Goal key={goal.id} {...goal} deleteGoal={props.deleteGoal}/></li>)
+    return (
+      <ol>	  
+        {goalsList}
       </ol>
-      </div>
     );
   }
-}
