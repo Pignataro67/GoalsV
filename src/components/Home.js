7197b2c308c3c36f14c0f530386f8d444ea04ba4
Pignataro67 <Pignataro67@gmail.com>
2020-05-22 09:54:38 -0700

    OI comp

--- src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Home = () => (
-  <div  className="home-page">
-    Home Page Content here
-  </div>
-);
-
-export default Home;
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import goalsReducer from './manageGoals';
-import objectivesReducer from './manageObjectives';
-
-const rootReducer = combineReducers({
-  goals: goalsReducer
-});
-
-export default rootReducer;
