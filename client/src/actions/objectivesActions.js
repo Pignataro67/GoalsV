9279461dacc4d77577a746f33b1033a2802005be
Pignataro67 <Pignataro67@gmail.com>
2020-05-23 09:14:02 -0700

    improving action func.

--- client/src/actions/objectivesActions.js
+++ client/src/actions/objectivesActions.js
@@ -1,10 +1,23 @@
 const goalsURL = "/api/goals";
 
-export function fetchObjectives(){
-  return (dispatch) => {
-    dispatch({type: "LOADING_OBJECTIVES"})
-      return fetch(`${goalsURL}/${goalID}/objectives`)
-        .then(response => response.json())
-        .then(objectives => dispatch({type: 'FETCH_OBJECTIVES', payload: objectives}))
+export const addObjective = ( objectiveInput, goalId ) => {
+let data = {
+method: 'POST',
+headers: {
+
+'Accept': 'application/json',
+
+'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(objectiveInput)
+  }
+  return dispatch => {
+    fetch(`${ goalsURL }/${goalId}/objectives`, data)
+      .then(response => response.json())
+      .then(objective => dispatch({
+        type: 'CREATE_OBJECTIVE',
+        payload: objective
+      }))
+      .catch(err => err)
   }
 }
--- client/src/reducers/manageObjectives.js
+++ client/src/reducers/manageObjectives.js
@@ -1,12 +1,16 @@
 export default function objectivesReducer(state = {
-  objectives: []}, 
+  loading: false,
+  objectivesData: []}, 
   action){
   switch(action.type){
-    case 'ADD_OBJECTIVE':
-      console.log('adding', action.objective )
-    return state
+    case 'LOADING_OBJECTIVES':
+      console.log("loading goals now")
+      return {...state, loading: true}
+    case 'FETCH_OBJECTIVES':
+      console.log('Fetching Objectives', action.payload )
+      return {loading: false, objectivesData: action.payload}
     default:
       console.log("hit default case in objective reducer")
-    return state;
+      return state;
   }
 }
