9df075307b5754cfd148e60b0f3582eb3c2d7f0f
Pignataro67 <Pignataro67@gmail.com>
2020-05-23 09:20:00 -0700

    refractoring

--- client/src/components/Goals/Goal.js
+++ client/src/components/Goals/Goal.js
@@ -3,12 +3,9 @@ import { Button, Grid } from 'semantic-ui-react';
 import ObjectivesContainer from '../../containers/ObjectivesContainer';
 
 class Goal extends React.Component { 
-  handleClick = () => {
-    this.props.deleteGoal(this.props.id)
-  }
 
   render() {
-    const {aim, strategy, category, key, objectives } = this.props;
+    const {aim, strategy, category, objectives, goal } = this.props;
     return(
       <div className="goal">
         <h3>{aim}</h3>
@@ -16,10 +13,10 @@ class Goal extends React.Component {
           <p>{strategy}</p>
         <strong>Category:</strong>
             <p>{category}</p>
-        <Button basic color='red' size='mini' onClick={() => this.props.deleteGoal(this.props.id)}>Delete</Button>
-        <p><ObjectivesContainer /></p>
+        <Button basic color='red' size='mini' onClick={ () => this.props.deleteGoal(this.props.id)}>Delete</Button>
+        <p><ObjectivesContainer goal={goal} objectives={objectives} /></p>
       </div>
-    )
+    );
   }
 }
 
