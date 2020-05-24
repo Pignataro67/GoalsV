58541dd0b929511ea882fb01b5a353e8c821e971
Pignataro67 <Pignataro67@gmail.com>
2020-05-24 08:05:45 -0700

    adding state

--- client/src/components/Objectives/ObjectiveInput.js
+++ client/src/components/Objectives/ObjectiveInput.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
 
-export default class ObjectiveInput extends Component {
+import { connect } from 'react-redux';
+import { Button, Form } from 'semantic-ui-react'
+import { addObjective } from '../../actions/objectivesActions'
+
+class ObjectiveInput extends Component {
+  constructor() {
+    super()
+    this.state = {
+      description: ""
+    }
+  }
+
+  handleChange = (e) =>{
+    this.setState({
+      [e.target.id] : e.target.value
+    })
+  }
+
+  handleOnSubmit = (e) => {
+    e.preventDefault();
+    this.props.addObjective(this.state, this.props.goalId);
+    this.setState({
+      description: ''
+    })
+  }
+
   render() {
     return (
       <div>Objective Form</div>
