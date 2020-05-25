2f302c4cbf0b79ab31892ca5d4d8ce5e8647872f
Pignataro67 <Pignataro67@gmail.com>
2020-05-25 14:50:35 -0700

    improving return

--- client/src/containers/ObjectivesContainer.js
+++ client/src/containers/ObjectivesContainer.js
@@ -2,19 +2,16 @@ import React, { Component } from 'react';
 import ObjectivesList from '../components/Objectives/ObjectivesList';
 import ObjectiveInput from '../components/Objectives/ObjectiveInput';
 // import { connect } from 'react-redux';
-// import { fetchObjectives, deleteObjective } from  '../actions/ObjectivesActions';
 
 class ObjectivesContainer extends Component {
 
   render(){
     return(
       <div>
-
-        <ObjectiveInput />
-        <ObjectivesList  /> 
-
+        <ObjectivesList goalId={this.props.goal.id} objectives={this.props.objectives} /> 
+        <ObjectiveInput goalId={this.props.goal.id}/>
       </div>
-    )
+    );
   }
 }
 
