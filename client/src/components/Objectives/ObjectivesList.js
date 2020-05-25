3e30f805c32bd83b53e5a956299b2299ac9e705f
Pignataro67 <Pignataro67@gmail.com>
2020-05-25 14:48:00 -0700

    returning ol

--- client/src/components/Objectives/ObjectivesList.js
+++ client/src/components/Objectives/ObjectivesList.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import Objective from './Objective';
 
-export default class ObjectivesList extends Component {
-  render() {
-    return (
-      <div> Objectives List </div>
-    )
-  }
+const ObjectivesList = props => {
+  const oList = props.objectives.map(objective =><li><Objective description={objective.description}/></li>)
+
+  return (
+    <div> <strong>TODO</strong>
+      <ul>
+        {oList}
+      </ul>
+    </div>
+  );
 }
+
+export default ObjectivesList; 
