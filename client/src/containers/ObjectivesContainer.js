7b24bac372718bd10b8c4d9bc4093987236b0127
Pignataro67 <Pignataro67@gmail.com>
2020-05-26 06:49:42 -0700

    adding to container

--- client/src/containers/ObjectivesContainer.js
+++ client/src/containers/ObjectivesContainer.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import ObjectivesList from '../components/Objectives/ObjectivesList';
+import Objectives from '../components/Objectives/Objectives';
 import ObjectiveInput from '../components/Objectives/ObjectiveInput';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
+import { addObjective } from '../actions/objectivesActions';
 
 class ObjectivesContainer extends Component {
 
   render(){
     return(
       <div>
-        <ObjectivesList goalId={this.props.goal.id} objectives={this.props.objectives} /> 
-        <ObjectiveInput goalId={this.props.goal.id}/>
+        Objectives Container
+        <ObjectiveInput addObjective={this.props.addObjective} goalId={this.props.goal.id}/>
       </div>
     );
   }
