a6be40b3105ebd5bc09677a81a3363c081090d2a
Pignataro67 <Pignataro67@gmail.com>
2020-05-26 06:45:34 -0700

    improving reducer

--- client/src/reducers/manageObjectives.js
+++ client/src/reducers/manageObjectives.js
@@ -6,9 +6,9 @@ export default function objectivesReducer(state = {
     case 'LOADING_OBJECTIVES':
       console.log("loading goals now")
       return {...state, loading: true}
-    case 'FETCH_OBJECTIVES':
-      console.log('Fetching Objectives', action.payload )
-      return {loading: false, objectivesData: action.payload}
+    case 'CREATE_OBJECTIVE':
+      console.log('cREATE Objectives', action.payload )
+      return {...state, objectivesData:[...state.objectivesData, action.payload]}
     default:
       console.log("hit default case in objective reducer")
       return state;
